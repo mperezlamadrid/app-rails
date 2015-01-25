4ed7536a5758e7150d35c828be4322e6e528fe30
Manuel <mperez@wawand.co>
2015-01-25 09:30:37 -0500

    change value for the database

--- db/migrate/20150124143253_create_contacts.rb
+++ db/migrate/20150124143253_create_contacts.rb
@@ -3,7 +3,7 @@ class CreateContacts < ActiveRecord::Migration
     create_table :contacts do |t|
       t.string :first_name
       t.string :last_name
-      t.integer :phone
+      t.integer :phone, :limit => 8
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20150124170347) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contacts", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.integer  "phone"
+    t.integer  "phone",      limit: 8
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -36,7 +39,7 @@ ActiveRecord::Schema.define(version: 20150124170347) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
